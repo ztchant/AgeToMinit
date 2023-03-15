87f135ef2864a51f4ea8eede74e02d9c5cdbc24f
zurab <zurabazura@gmail.com>
2023-03-15 21:52:45 +0400

    AgeToMinutes

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.myapplication
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.DatePicker
 import android.widget.TextView
 import android.widget.Toast
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
-    var minutes : TextView?= null
+    var minutes: TextView? = null
+    var entrDate :TextView? =null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val dateButton: Button = findViewById(R.id.buttonPickDate)
-        val date: TextView = findViewById(R.id.enteredDate)
-        minutes =findViewById(R.id.textView9)
+        minutes = findViewById(R.id.resultMinute)
+        entrDate= findViewById(R.id.enteredDate)
 
         dateButton.setOnClickListener {
             openCalendar()
@@ -23,12 +29,31 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-     fun openCalendar() {
-        minutes?.text = "03.07.2023"
-        Toast.makeText(this, "this button work slay", Toast.LENGTH_SHORT).show()
+    fun openCalendar() {
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        val carentYear=2023
+        val carentDay = 15
+        val carentMonth = 3
+        val answer =(((carentYear-year)*12+(carentMonth-month))*30-(day-carentDay))*24*60
+
+        DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                Toast.makeText(this, "calendar is working", Toast.LENGTH_LONG).show()
+
+            },
+            year,
+            month,
+            day
+
+        ).show()
     }
 
-}
+
+    }
 
 
 
